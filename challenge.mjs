f110e45f178dcaaaab5e9a2409abfbd9e46f625b
pabel1 <pabelahamed39@gmail.com>
2024-01-13 21:08:27 +0600

    solve logic-transformer

--- challenge.mjs
+++ challenge.mjs
@@ -1,3 +1,45 @@
+const transformFunc = (weirdExpression) => {
+  const parts = weirdExpression.split(" ");
+  const operator = {
+    ebong: "&&",
+    othoba: "||",
+  };
+  const transformParts = parts.map((part) => operator[part] || part);
+  let transformedExpression = transformParts.join(" ");
+  // Handle special cases:
+  if (transformedExpression === "&&") {
+    transformedExpression = "ebong";
+  } else if (transformedExpression === "||") {
+    transformedExpression = "othoba";
+  } else if (
+    transformedExpression.includes("&& &&") ||
+    transformedExpression.includes("|| ||")
+  ) {
+    transformedExpression = transformedExpression.replaceAll("&&", "ebong");
+    transformedExpression = transformedExpression.replaceAll("||", "othoba");
+    transformedExpression = transformedExpression.replaceAll(
+      "ebong ebong",
+      "ebong &&"
+    );
+    transformedExpression = transformedExpression.replaceAll(
+      "othoba othoba",
+      "othoba ||"
+    );
+  } else if (transformedExpression.endsWith("||")) {
+    const newExpression = transformedExpression.split(" ");
+    newExpression.pop();
+    newExpression.push("othoba");
+    transformedExpression = newExpression.join(" ");
+  } else if (transformedExpression.endsWith("&&")) {
+    const newExpression = transformedExpression.split(" ");
+    newExpression.pop();
+    newExpression.push("ebong");
+    transformedExpression = newExpression.join(" ");
+  }
+
+  return transformedExpression;
+};
+
 const weirdExpressions = [
   "a othoba b",
   "b ebong c othoba d",
@@ -8,5 +50,6 @@ const weirdExpressions = [
 ];
 
 for (const expression of weirdExpressions) {
-  // fill this in
+  const result = transformFunc(expression);
+  console.log(result);
 }
